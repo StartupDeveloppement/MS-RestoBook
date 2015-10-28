13e49c3655fa22190f67bb75dbe3d27341448955
RomainRivolta <romain.rivolta@gmail.com>
2015-10-28 11:04:02 +0100

    Bootstrap

---
+++ RestoBook/RestoBook/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Optimization;
+
+namespace RestoBook
+{
+    class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new StyleBundle("~/Content/Css").Include(
+            "~/Content/Css/bootstrap.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            "~/Scripts/modernizr-{version}.js"));
+
+        }
+    }
+}

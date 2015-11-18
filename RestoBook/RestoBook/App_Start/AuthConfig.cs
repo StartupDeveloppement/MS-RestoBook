5b0149a5c84e1e474364cae8733e9964b7c35634
Abddoul78 <Abdel_78180@hotmail.fr>
2015-11-18 15:06:54 +0100

    connection

---
+++ RestoBook/RestoBook/App_Start/AuthConfig.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: OwinStartup(typeof(RestoBook.AuthConfig))]
+namespace RestoBook
+{
+    public class AuthConfig
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Authentication/Login")
+            });
+        }
+    }
+}

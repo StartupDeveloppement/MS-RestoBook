1d94c2eff21f85f5f40f7dbf08ea082607819fa8
RomainRivolta <romain.rivolta@gmail.com>
2016-03-23 13:46:55 +0100

    Commit WPF

---
+++ RestoBook/RestoBook.Wpf/Common/Anonymous_Class/InfoRestaurant.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoBook.Common.Anonymous_Class
+{
+    class InfoRestaurant
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Rue { get; set; }
+        public string Cp { get; set; }
+        public bool? Active { get; set; }
+    }
+}
---
+++ RestoBook/RestoBook.Wpf/ViewModel/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoBook.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
